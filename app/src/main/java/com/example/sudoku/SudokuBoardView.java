613a6ecad280e243f409f539786f08b3fb27b6c0
moros101 <mohitgoswami16@gmail.com>
2023-02-16 18:19:26 +0530

    1-9 button grid view

---
+++ app/src/main/java/com/example/sudoku/SudokuBoardView.java
@@ -0,0 +1,17 @@
+package com.example.sudoku;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+
+
+public class SudokuBoardView {
+    Context context;
+    AttributeSet attributeSet;
+    private String thickLinePaint = Paint().apply{
+
+    }
+    public void onDraw(Canvas canvas){
+
+
+    }
+}
