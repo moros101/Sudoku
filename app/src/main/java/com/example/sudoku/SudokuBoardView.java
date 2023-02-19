63c657d1d93ffe8b6af9ffae0613297f9e165ede
moros101 <mohitgoswami16@gmail.com>
2023-02-19 11:15:30 +0530

    sudoku board border

--- app/src/main/java/com/example/sudoku/SudokuBoardView.java
+++ app/src/main/java/com/example/sudoku/SudokuBoardView.java
@@ -1,17 +1,48 @@
 package com.example.sudoku;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
 
 
-public class SudokuBoardView {
-    Context context;
-    AttributeSet attributeSet;
-    private String thickLinePaint = Paint().apply{
+public class SudokuBoardView extends View{
+
+    private final int boardColor;
+    private final Paint boardColorPaint = new Paint();
+    public SudokuBoardView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,R.styleable.SudokuBoardView,0,0);
+
+        try{
+            boardColor = a.getInteger(R.styleable.SudokuBoardView_boardColor,0);
+
+        }finally {
+            a.recycle();
+        }
+    }
+
+    @Override
+    protected void onMeasure(int width, int height){
+        super.onMeasure(width,height);
+
+        int dimension = Math.min(this.getWidth(),this.getHeight());
+        setMeasuredDimension(dimension,dimension);
 
     }
-    public void onDraw(Canvas canvas){
 
+    @Override
+    protected void onDraw(Canvas canvas){
 
+        boardColorPaint.setStyle(Paint.Style.STROKE);
+        boardColorPaint.setStrokeWidth(16);
+        boardColorPaint.setColor(boardColor);
+        boardColorPaint.setAntiAlias(true);
+
+        canvas.drawRect(0,0,getWidth(),getHeight(),boardColorPaint);
     }
 }
